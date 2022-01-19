c78d13cf8929f859e5bea4fb985ae966683aa34b
Jade Meskill <jade.meskill@gmail.com>
2022-01-19 20:36:00 +0000

    matrix-cli: add 0.0.2 bottle.

--- Formula/matrix-cli.rb
+++ Formula/matrix-cli.rb
@@ -6,6 +6,12 @@ class MatrixCli < Formula
   license "MIT"
   head "https://github.com/iamruinous/matrix-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/iamruinous/matrix-tools"
+    sha256 cellar: :any_skip_relocation, big_sur:      "598e74c3623d7072a2ec5934a04a8ecd83e4f60a302209713f8f8443f29d8a7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6468e610fd92310aa0f52bbf8a9b6d163a05c143f901e7b7a6917406140cec8b"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
